43dc746f680f6e29e269e8185367cd461f24a73e
Iago_saito <gokuiago@gmail.com>
2021-10-28 13:44:57 -0300

    Arquitetura em notebook descontinuada.

---
+++ ChatBot/dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def data_set_padrao():
+    url = "https://raw.githubusercontent.com/Iagoakiosaito/IA-ChatBot-UFMS/main/Chatter-DB.csv"
+    df=pd.read_csv(url)
+    df = df[df['Orador'].notna()]
+    df = df[df['Sentença'].notna()]
+    df = df[df['Intenção'].notna()]
+    df = df[(df.Orador == '<')]
+    
+    return df
+
+
+
